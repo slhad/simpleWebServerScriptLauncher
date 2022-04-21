c764bfa1e36ac2b8d82439f1c9c267b066038446
Sylvain Rivierre <sylvainrivierre@gmail.com>
2022-04-22 00:29:29 +0200

    bin

--- index.ts
+++ index.ts
@@ -8,32 +8,37 @@ const script = process.env.script || "script.sh"
 const method = process.env.method || "post"
 const path = process.env.urlpath || "runScript"
 const port = process.env.port || "8888"
+const address = process.env.address || "0.0.0.0"
 const responseType = "text/plain"
 
+const listen = () => {
+    createServer(async (req, res) => {
+        console.log(`Query ${req.method} ${req.url}`)
+        if (
+            ((req.method || "post").toLocaleLowerCase() === method?.toLocaleLowerCase())
+            && ((req.url || "/") === `/${path}`)
+        ) {
 
-const executeCommand = async () => {
-
+            try {
+                const execution = await exec(script)
+                res.writeHead(200, { 'Content-Type': responseType });
+                res.write(`Script ${script} executed :\nstdout :\n${execution.stdout}\nstderr:\n${execution.stderr}`)
+            } catch ({ message, stack }) {
+                res.writeHead(500, { 'Content-Type': responseType });
+                res.write(`Script ${script} executed and failed : ${message}\n${stack}`)
+            }
+        } else {
+            res.writeHead(404, { 'Content-Type': responseType });
+            res.write(`Rejected query ${req.method} ${req.url}`);
+        }
+        res.end();
+    }).listen(parseInt(port), address, undefined, () => {
+        console.log(`Will listen for ${method} ${port} ${path} -> ${script}`)
+    });
 }
 
-console.log(`Will listen for ${method} ${port} ${path} -> ${script}`)
-createServer(async (req, res) => {
-    console.log(`Query ${req.method} ${req.url}`)
-    if (
-        ((req.method || "post").toLocaleLowerCase() === method?.toLocaleLowerCase())
-        && ((req.url || "/") === `/${path}`)
-    ) {
+export default listen
 
-        try {
-            const execution = await exec(script)
-            res.writeHead(200, { 'Content-Type': responseType });
-            res.write(`Script ${script} executed :\nstdout :\n${execution.stdout}\nstderr:\n${execution.stderr}`)
-        } catch ({ message, stack }) {
-            res.writeHead(500, { 'Content-Type': responseType });
-            res.write(`Script ${script} executed and failed : ${message}\n${stack}`)
-        }
-    } else {
-        res.writeHead(404, { 'Content-Type': responseType });
-        res.write(`Rejected query ${req.method} ${req.url}`);
-    }
-    res.end();
-}).listen(port);
+if (!module.require.main) {
+    listen()
+}
